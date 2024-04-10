47ca011e8e651dbe41164e1786533650a81f5531
keyhan azarjoo <keyhanazarjoo@gmail.com>
2024-04-10 14:15:18 +0100

    Update Singleton.cs

--- Singleton.cs
+++ Singleton.cs
@@ -1,4 +1,12 @@
 // Implementing Singleton Design Pattern
+//In the Singleton pattern, a class is designed to have only one instance, and that instance provides a global point of access to it. This is useful when exactly one object is needed to coordinate actions across the system.
+
+//In the code:
+
+//The class MySingletonClass has a private constructor, preventing other classes from instantiating it directly.
+//It has a static method GetInstance() that provides the global access point to the singleton instance.
+//It maintains a private static instance MSC of the class, which is initialized only once when the class is loaded.
+//This ensures that only one instance of MySingletonClass exists throughout the lifetime of the program, and it can be accessed globally through the GetInstance() method.
 
 using System;
 
@@ -27,3 +35,4 @@ public class MySingletonClass
         return MSC;
     }
 }
+
